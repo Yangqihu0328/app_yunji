2ef1171e69880a4edc06e5050c46f07151ab1682
Yang Qi Hu <187205342@qq.com>
2024-11-08 18:21:03 +0800

    update code

--- demo/src/ppl/box/box_ppl/BoxConfig.hpp
+++ demo/src/ppl/box/box_ppl/BoxConfig.hpp
@@ -121,11 +121,8 @@ public:
     COMPRESS_CONFIG_T GetCompressConfig(AX_VOID);
     UT_CONFIG_T GetUTConfig(AX_VOID);
 
-    AX_BOOL AddStreamUrl(AX_S32 channelId, std::string& channelUrl);
-    AX_BOOL RemoveStreamUrl(AX_S32 channelId);
     AX_BOOL AddAlgoTask(AX_S32 channelId, std::vector<int> &task_vec);
     AX_BOOL RemoveAlgoTask(AX_S32 channelId);
-    AX_BOOL GetStreamCount(AX_S32 &count);
 
 private:
     CBoxConfig(AX_VOID) = default;
